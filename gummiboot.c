de7bef4c2d3280d53c536b63027c77680a357e20
Kay Sievers <kay@vrfy.org>
2012-07-13 20:00:52 +0200

    update README

--- gummiboot.c
+++ gummiboot.c
@@ -3,6 +3,9 @@
  * default entry is selected by a configured pattern (glob) or an on-screen
  * menu.
  *
+ * All gummiboot code is LGPL not GPL, to stay out of politics and to give
+ * teh freedom of copying code from programs to possible future libraries.
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or
